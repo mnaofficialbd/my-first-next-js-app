0e166172bf523c93f501849d2697c6aecdb44a42
mnaofficialbd <mna.official92@gmail.com>
2024-01-02 11:29:19 +0600

    passing function

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,14 @@
+"use client";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const apple = (item) => {
+    alert(item);
+  };
   return (
     <main className={styles.main}>
       <h1>Home page</h1>
-      <User name="azam" />
-      <User name="nurul" />
-      <User name="rafi" />
-      <User name="munna" />
+      <button onClick={() => apple("mna")}>Click</button>
     </main>
   );
 }
-
-const User = (props) => {
-  return (
-    <div>
-      <h2>My name is {props.name} </h2>
-    </div>
-  );
-};
