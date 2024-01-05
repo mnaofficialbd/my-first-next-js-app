b4ba2ea641e1c0987d99a5accf4e721d8b997939
mnaofficialbd <mna.official92@gmail.com>
2024-01-05 20:41:30 +0600

    data passing with component

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,27 @@
 import styles from "./page.module.css";
 
 export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Home page</h1>
+      <User name="azam" />
+      <User name="nurul" />
+      <User name="rafi" />
+      <User name="munna" />
+      <button onClick={() => apple("mna")}>Click</button>
+    </main>
+  );
+}
+
+const User = (props) => {
+  return (
+    <div>
+      <h2>My name is {props.name} </h2>
+    </div>
+  );
+};
+
+/* export default function Home() {
   const apple = (item) => {
     alert(item);
   };
@@ -11,4 +32,4 @@ export default function Home() {
       <button onClick={() => apple("mna")}>Click</button>
     </main>
   );
-}
+} */
